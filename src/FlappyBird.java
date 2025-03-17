d0de105dbdabccb2dc2e262223bee01ad97c33d6
nicholas-garcia-scholtz <ngar756@aucklanduni.ac.nz>
2025-03-17 22:21:29 +1300

    Drew the background image

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,18 +1,40 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList; // Going to store all the pipes
+import java.util.ArrayList; // going to store all the pipes
 import java.util.Random; // to place pipes at random positions
 import javax.swing.*;
 
 public class FlappyBird extends JPanel{
-// Adding JPanel to use for our canvas (to draw our game)
 
     int boardWidth = 360;
     int boardHeight = 640;
 
+
+    // Images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight)); //create canvas
-        setBackground(Color.blue);
+        // setBackground(Color.blue);
+
+        //load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage(); //getClass refers to flappy bird class
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        // draw background
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
     }
 
     
